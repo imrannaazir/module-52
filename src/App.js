d90f5f8d4eb36613aed241aa01b125d87138e234
imrannaazir <imrannaaziremon@gmail.com>
2022-03-30 16:06:59 +0600

    route add in the site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Friends from './componets/Friends/Friends';
+import About from './componets/About/About';
+import { Route, Routes } from 'react-router-dom';
+import Home from './componets/Home/Home';
+import NOF from './componets/NOF/NOF';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello routers world .</h1>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/friends' element={<Friends />}></Route>
+        <Route path='*' element={<NOF></NOF>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/componets/Friends/Friends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h1>Here are all friends of you.</h1>
+        </div>
+    );
+};
+
+export default Friends;
